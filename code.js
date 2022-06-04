a3ba164918a126662e3bf017d5e648a04b1e0538
augustoreis8 <augustocrisostomo8@gmail.com>
2022-06-04 17:01:36 -0300

    Possível código final da tarefa de Try caych

---
+++ code.js
@@ -0,0 +1,42 @@
+const inputFileButton = document.getElementById("inputfilebutton");
+inputFileButton.addEventListener("click", clickFileButton);
+
+const inputNumberButton = document.getElementById("inputnumbersbutton");
+inputNumberButton.addEventListener("click", clickNumbersButton);
+
+function clickFileButton() {
+    arquivo = document.getElementById("inputfile");
+
+    var fr = new FileReader();
+
+    try {
+        fr.readAsText(arquivo.files[0]);
+        fr.onload = function () {
+            info = fr.result;
+            document.getElementById('output').textContent = fr.result;
+        }
+    }
+    catch (erro) {
+        console.log(erro);
+        alert(erro);
+    }
+    finally {
+        alert("Obrigado pela visita");
+    }
+}
+
+function clickNumbersButton() {
+    numbers = document.getElementById("inputnumbers").value
+
+    try {
+        if (numbers == '') throw 'Informe um valor';
+
+        if (numbers < 5 || numbers > 10) throw 'Informe um valor maior que 5 e menor que 10'
+    }
+    catch (erro) {
+        document.getElementById("outputnumbers").innerHTML = erro;
+    }
+    finally {
+        alert("o numero escolhido foi: " + numbers);
+    }
+}
